59e1bc0ded89fca2371c0681fdad02a68fdfec9e
GiFiSi <gifisi.channnel@gmail.com>
2025-01-20 09:01:45 -0300

    Adicionado lembrete para commit dentro do VSCode

---
+++ 1.0/main.py
@@ -0,0 +1,11 @@
+""" 1°Passo acessar terminal e selecionar Git Bash
+
+2°Passo executar git add --all para selecionar todas alterações
+
+3°Passo executar git status para verificar esta tudo ok
+
+4°Passo executar git commit -m 'frase desejada'
+
+5°Passo executar git push
+
+6°Passo executar git status verificar se deu tudo certo """
